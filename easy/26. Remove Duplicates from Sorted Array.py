770d3102ad88ff09c54e6db9b17eef330b946774
Vishal Mishra <vishal.mishra@geodis.com>
2026-05-16 16:24:56 -0500

    Remove Duplicates from Sorted Array

---
+++ easy/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,78 @@
+"""
+26. Remove Duplicates from Sorted Array
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+
+"""
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        ## Method 1 TC O(n) and SC O(n)
+        # result = []
+        # for i in nums:
+        #     if i not in result:
+        #         result.append(i)
+        # return result
+        ## Method 2 TC O(n) and SC O(1)
+        L, R = 0, 1
+        while R < len(nums):
+            if nums[L] == nums[R]:
+                del nums[R]
+            else:
+                L += 1
+                R += 1
+        return len(nums) # if list is required then return nums
+
+
+s = Solution()
+print(s.removeDuplicates([1,1,2])) #2, nums = [1,2]
+print(s.removeDuplicates([0,0,1,1,1,2,2,3,3,4])) #5, nums = [0,1,2,3,4]
+print(s.removeDuplicates([1,1,1,1,1,1,1,1,1,1])) # 1, nums = [1]
+print(s.removeDuplicates([1,1,2,3,4])) # 4, nums = [1,2,3,4]
+print(s.removeDuplicates([2,10,10,30,30,30])) # 3, nums = [2,10,30]
