ad3934df78039472ad691d83878c90fa148f3127
Vishal Mishra <vishal.mishra@geodis.com>
2026-06-05 09:40:37 -0500

    Climbing Stair

--- easy/70. Climbing Stairs.py
+++ easy/70. Climbing Stairs.py
@@ -36,7 +36,7 @@ class Solution:
         # dynamic programming approach with backtracking
         one = 1
         two = 1
-        for i in range(2, n + 1):
+        for _ in range(2, n + 1):
             temp = one
             one = one + two
             two = temp
